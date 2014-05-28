fe1d33c7726f05c4a6f389bf8648bc642690f8ed
David Kesler <keslerdr@gmail.com>
2014-05-27 22:39:27 -0400

    Added command line args
    The puzzle file to be used is passed in via command line instead of
    hardcoded in the solver's main method.

--- src/main/scala/RegexCrosswordSolver.scala
+++ src/main/scala/RegexCrosswordSolver.scala
@@ -2,7 +2,10 @@ import java.io.File
 
 object RegexCrosswordSolver extends App {
   override def main(args: Array[String]): Unit = {
-    val crossword = PuzzleReader.read(new File("src/main/resources/puzzles/regexcrossword/intermediate-5.dat"))
+    val url = getClass.getResource(args(0))
+    if (url == null) throw new Error("Could not locate file " + args(0))
+    val file = new File(url.getFile)
+    val crossword = PuzzleReader.read(file)
     val sol = crossword.solve()
 
     sol match {
